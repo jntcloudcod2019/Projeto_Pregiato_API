67e28555ec2401408c4458cfba8defa9d0f91bd7
Jonathan Fernando <57296925+jntcloudcod2019@users.noreply.github.com>
2025-05-02 17:30:49 -0300

    dna mais uma vez

---
+++ Pregiato.API/System/Text/Json/Serialization/CaseInsensitiveJsonConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Pregiato.API.System.Text.Json.Serialization
+{
+    public class CaseInsensitiveJsonConverter<T> : JsonConverter<T> where T : class
+    {
+        public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            // Clona os options para não alterar os globais
+            var customOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            foreach (var converter in options.Converters)
+            {
+                customOptions.Converters.Add(converter);
+            }
+
+            return JsonSerializer.Deserialize<T>(ref reader, customOptions);
+        }
+
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, value, options);
+        }
+    }
+}
