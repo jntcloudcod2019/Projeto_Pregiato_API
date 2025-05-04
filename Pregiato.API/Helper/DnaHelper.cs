89f0473f3bf39603238c7e1fca023d96353fc896
Jonathan Fernando <57296925+jntcloudcod2019@users.noreply.github.com>
2025-05-04 17:51:09 -0300

    correção e refatoração models dna

---
+++ Pregiato.API/Helper/DnaHelper.cs
@@ -0,0 +1,27 @@
+﻿using Pregiato.API.Models;
+
+namespace Pregiato.API.Helper
+{
+    public static class DnaHelper
+    {
+        public static void EnsureDefaults(ModelDnaData attributes)
+        {
+            attributes.Appearance ??= new Appearance();
+
+            attributes.Appearance.Eyes ??= new EyeAttributes();
+            attributes.Appearance.Hair ??= new HairAttributes();
+            attributes.Appearance.Skin ??= new SkinAttributes { Marks = new List<string>() };
+            attributes.Appearance.Face ??= new FaceAttributes();
+            attributes.Appearance.Smile ??= new SmileAttributes();
+            attributes.Appearance.Body ??= new BodyAttributes();
+
+            attributes.AdditionalAttributes ??= new AdditionalAttributes
+            {
+                Skills = new List<string>(),
+                Experience = new List<string>()
+            };
+
+            attributes.PhysicalCharacteristics ??= new PhysicalCharacteristics();
+        }    
+    }
+}
